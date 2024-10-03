b5c33f6d63d45cb6467730106c8d2e4b99736478
GuilhermeACSM <zguilhermegn@gmail.com>
2024-10-03 09:50:37 -0300

    Atualizado

--- Ex-Carros.sql
+++ Ex-Carros.sql
@@ -7,9 +7,12 @@ ano date not null,
 preco float(9,2) default 0
 );
 
+alter table tb_carros add column cor varchar(50); -- Adicionando coluna na tabela!!
+alter table tb_carros add column portas varchar(10); -- Adicionando coluna na tabela!! 
+
 drop table tb_carros; -- Para limpar a tabela toda!!!
 alter table tb_carros change ano ano int not null; -- Mudando os type da coluna da tabela!!
-describe tb_carros; -- Mostrar a descrição da taabela!!
+describe tb_carros; -- Mostrar a descrição da tabela!!
 
 insert into tb_carros (id,marca,modelo,ano,preco) values -- Continuação na linha de baixo!
 (1,'Toyota','Corolla','2020','85000'), -- Continuação na linha de baixo!
@@ -17,5 +20,16 @@ insert into tb_carros (id,marca,modelo,ano,preco) values -- Continuação na lin
 (3,'Ford','Mustang','2021','300000'), -- Continuação na linha de baixo!
 (4,'Chevrolet','Onix','2018','45000');  -- INSERT INTO para adiconar dados fixos na tabela!!!
 
-truncate table tb_carros; -- Para limpar os dados inseridos pelo insert into na tabela!!!
+-- Para atualizar um dado específico da tabela!!!
+update tb_carros set cor='Vermelho', portas='4' where id between 1 and 100; -- Atualiza os dados com id de 1 até 100
+update tb_carros set cor='Rosa', portas='4' where id=1;-- Atualiza dados somente da id=1
+update tb_carros set cor='Preto', portas='4' where id=2; -- Atualiza dados somente da id=2
+update tb_carros set cor='Verde', portas='4' where id=3; -- Atualiza dados somente da id=3
+update tb_carros set cor='Amarelo', portas='4' where id=4; -- Atualiza dados somente da id=4
+
+select * from tb_carros where id=1; -- Para mostrar um dado específico na tabela(Insert into)!!!
+select marca, cor from tb_carros; -- Para mostrar os dados inseridos pelo insert into!!!
 select * from tb_carros; -- Para mostrar os dados inseridos pelo insert into!!!
+
+truncate table tb_carros; -- Para limpar os dados inseridos pelo insert into na tabela!!!
+delete from tb_carros where id=2; -- Para apagar uma certa linha dos dados insert!!!
