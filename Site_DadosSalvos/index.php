72ec8e6fbe817d0d7d8a195ddd5e586925d918a8
GuilhermeACSM <zguilhermegn@gmail.com>
2024-10-24 11:56:44 -0300

    Atualizado

---
+++ Site_DadosSalvos/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Site de Salvamento de Dados</title>
+    
+</head>
+<body>
+    <div class="titulo">
+        <h1>Salvamento de Dados</h1>
+    </div>
+
+    <div class="forms">
+        
+        <div class="inputs">
+            <label for="text">Serviço/Site: </label>
+            <input type="text" placeholder="Nome do servico/site">
+        </div>
+
+        <div class="inputs">
+            <label for="email">Login: </label>
+            <input type="email" placeholder="E-mail">
+        </div>
+        
+        <div class="inputs">
+            <label for="password">Senha: </label>
+            <input type="password" placeholder="Senha">
+        </div>
+
+        <button type="submit" class="adicionar">Adicionar</button>
+    </div>
+
+
+    <script src="script/script.js" defer></script>
+</body>
+</html>
+
